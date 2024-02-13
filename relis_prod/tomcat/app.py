e3798c9e0a30d7ae2b4b1bfb683c3a490e0281b5
gauranshkumar <gauranshk21@gmail.com>
2024-02-13 14:27:30 -0500

    Initial work on Tomcat API

--- relis_prod/tomcat/app.py
+++ relis_prod/tomcat/app.py
@@ -3,36 +3,42 @@ import os
 from fastapi.responses import JSONResponse, FileResponse
 import uvicorn
 
-app = FastAPI()
+app = FastAPI(docs_url='/apis/tomcat/docs')
 
 
-@app.get("/relis/texteditor/apis/list_projects")
+@app.get("/apis/tomcat/list_projects")
 def list_projects():
     # Your code to list projects goes here
-    response = {}
-    projects = os.listdir("/u/relis/public_html/workspace")
-    for project in projects:
-        response[project.strip()] = {
-            "files": [
-                i
-                for i in os.listdir(
-                    f"/u/relis/public_html/workspace/{project.strip()}/src-gen"
-                )
-                if i.endswith(".php")
-            ]
-        }
-    return JSONResponse(response)
+    try:
+        response = {}
+        projects = os.listdir("/u/relis/public_html/workspace/dslforge_workspace")
+        for project in projects:
+            response[project.strip()] = {
+                "files": [
+                    i
+                    for i in os.listdir(
+                        f"/u/relis/public_html/workspace/dslforge_workspace/{project.strip()}/src-gen"
+                    )
+                    if i.endswith(".php")
+                ]
+            }
+        return JSONResponse(response)
+    except Exception as e:
+        return JSONResponse({"error": str(e)})
 
 
-@app.get("/relis/texteditor/apis/get_project_configuration")
+@app.get("/apis/tomcat/get_project_configuration")
 def get_project_configuration(project_name: str, file_name: str):
     # Your code to get project configuration goes here
-    return FileResponse(
-        path=f"/u/relis/public_html/workspace/{project_name}/{file_name}",
-        media_type="text/plain",
-        filename=file_name,
-    )
+    try:
+        return FileResponse(
+            path=f"/u/relis/public_html/workspace/dslforge_workspace/{project_name}/{file_name}",
+            media_type="text/plain",
+            filename=file_name,
+        )
+    except Exception as e:
+        return JSONResponse({"error": str(e)})
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=8081, host="0.0.0.0", log_level="info")
+    uvicorn.run(app, port=8181, log_level="info")
