6136fe07d60667babbf55e86e3a9670a8799ee87
gauranshkumar <gauranshk21@gmail.com>
2024-06-18 15:24:26 -0400

    Hide deprecation warning in prod

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ switch (ENVIRONMENT) {
 		if (version_compare(PHP_VERSION, '5.3', '>=')) {
 			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
 		} else {
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 		}
 		break;
 
