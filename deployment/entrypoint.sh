b3bde39d859c41ea9712c09d6b3451f257395578
gauranshkumar <gauranshk21@gmail.com>
2024-07-16 17:30:41 -0400

    reset file permissions

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-chown -R www-data:www-data /u/relis/public_html/cside/export_r /u/relis/public_html/cside/export_python
-chown -R www-data:www-data /u/relis/public_html/cside/sessions
-chown -R www-data:www-data /u/relis/public_html/relis_app/
-
-chmod -R 664 /u/relis/public_html/cside/export_r /u/relis/public_html/cside/export_python
-chmod -R 664 /u/relis/public_html/cside/sessions
-chmod -R 664 /u/relis/public_html/relis_app/
+chmod -R 777 /u/relis/public_html/cside/export_r /u/relis/public_html/cside/export_python
+chmod -R 777 /u/relis/public_html/cside/sessions
+chmod -R 777 /u/relis/public_html/relis_app/config
 
 cd /u/relis/public_html/relis_app/config  && cp --no-clobber database.example.php database.php
 
