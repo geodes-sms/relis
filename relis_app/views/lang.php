668c9e69d2cf6df123273bfb505b7133beb05b00
brice <bricemichelb@gmail.com>
2024-11-26 10:24:15 -0500

    refactoring

--- relis_app/views/lang.php
+++ relis_app/views/lang.php
@@ -5,42 +5,32 @@
 	$label="en";
 	$title="Switch to French ";
 	
-	if($this->session->userdata('active_language') AND $this->session->userdata('active_language')=='fr' ){
-		
+	if($this->session->userdata('active_language') AND $this->session->userdata('active_language')=='fr' ) {
 		$fr_checked="checked";
 		$en_checked="";
 		$label="fr";
 		$title="Switch to English ";
 	}
-	
 	?>
-        <span  class="change_lang button_lang"><br/>
+    <span  class="change_lang button_lang"><br/>
         <a title="<?php echo $title ?>"  href="#" onclick="change_language()">
-       
-        <?php echo $label?>
-         </a> &nbsp
-         </span>
-        <!-- /page content -->
-         <script>
-                  
-                	
-                 function change_language( ){
-                    	
-                		//alert(lang);
-                	         $.ajax({
-                	     type: "POST",
-                	 url: "<?php echo base_url();?>home/change_lang/",
-                        	 
-                	 data: $('form.form_content').serialize(),
-                	         success: function(msg){
-                	               location.reload(); 
-                	               
-                	        
-                	         },
-                	 error: function(){
-                	 alert("loading failure");
-                	 }
-                	       });
-                	 }
-                	</script>
+            <?php echo $label; ?>
+        </a> &nbsp
+    </span>
+    <!-- /page content -->
+    <script>
+        function change_language( ) {
+            $.ajax({
+                type: "POST",
+                url: "<?php echo base_url();?>home/change_lang/",
+                 data: $('form.form_content').serialize(),
+                         success: function(msg){
+                               location.reload();
+                         },
+                 error: function(){
+                    alert("loading failure");
+                 }
+            });
+        }
+    </script>
                   
